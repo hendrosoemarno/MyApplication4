11d7010d62f4204cf15502e66aba9dffbf2a581d
DESKTOP-AS2327L\SARPRA PROGRAMER <hendrosoemarno@gmail.com>
2017-08-08 09:20:46 +0700

    tes 2

--- app/src/main/java/com/hello/sarpraprogramer/myapplication/MainActivity.java
+++ app/src/main/java/com/hello/sarpraprogramer/myapplication/MainActivity.java
@@ -1,13 +1,57 @@
 package com.hello.sarpraprogramer.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*logt lalu tab */
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /*ctrl + shift + / shortcut comment multi baris*/
+   /*ctrl o keluar override*/
+    @Override
+    protected void onStart() {
+        super.onStart();
+         /*logd lalu tab */
+        Log.d(TAG, "onStart: sudah start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: sudah resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: sudah pause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: sudah restart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: sudah stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: sudah destroy");
     }
 }
