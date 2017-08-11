41475f899c51a580e9cddacb00e8d32fbbab2924
DESKTOP-AS2327L\SARPRA PROGRAMER <hendrosoemarno@gmail.com>
2017-08-11 14:10:28 +0700

    my6

--- app/src/main/java/com/hello/sarpraprogramer/myapplication/KonfirmActivity.java
+++ app/src/main/java/com/hello/sarpraprogramer/myapplication/KonfirmActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,6 +38,35 @@ public class KonfirmActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_moklet,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch(itemId){
+            case R.id.share:
+                share();
+                break;
+            case R.id.logout:
+                Toast.makeText(KonfirmActivity.this,"log out",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    private void share() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        //startActivity(sendIntent);
+        startActivity(Intent.createChooser(sendIntent,"coba" ));
+        Toast.makeText(KonfirmActivity.this,"share",Toast.LENGTH_SHORT).show();
+    }
 
     public void iCall(View view) {
         Toast.makeText(this,"iCall",Toast.LENGTH_SHORT).show();
@@ -54,12 +85,4 @@ public class KonfirmActivity extends AppCompatActivity {
         }
     }
 
-    public void iShare(View view) {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-        sendIntent.setType("text/plain");
-        //startActivity(sendIntent);
-        startActivity(Intent.createChooser(sendIntent,"coba" ));
-    }
 }
--- app/src/main/java/com/hello/sarpraprogramer/myapplication/MainActivity.java
+++ app/src/main/java/com/hello/sarpraprogramer/myapplication/MainActivity.java
@@ -49,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+
     private void pOK() {
 
 
